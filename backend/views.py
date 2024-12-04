01d8ba1aed70322ac91f2721ac48c0826e2de303
Adnan Karim <adnan.karim@showpass.com>
2024-12-04 00:21:42 -0700

    more initial setups

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponse
+import stripe
+import io
+import os
+from dotenv import load_dotenv
+from collections import defaultdict
+from django.views.decorators.http import require_http_methods
+from django.core.files.storage import default_storage
+import time
+import re
+import json
+from supabase import create_client, Client
+import hashlib
+from collections import Counter
+from django.db.models import Q
+from django.utils import timezone
+from django.forms.models import model_to_dict
+from django.core import serializers
+from django.contrib.auth import authenticate, login
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from langchain_openai import ChatOpenAI
+from langchain.agents.agent_types import AgentType
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+import pickle
+from .serializers import (
+    UserSerializer,
+)
 
-# Create your views here.
+from .models import (
+    CustomUser
+)
+# from langchain.vectorstores import FAISS
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from rest_framework import status
+from rest_framework.response import Response
+import jwt
+import secrets
+import googlemaps
+import requests
+from django.conf import settings
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta
+from token_count import TokenCount
+from datetime import datetime
+import pytz
+from twilio.rest import Client
